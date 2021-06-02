483059253070354cdea4834c826133f0c53ae70f
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-06-01 23:41:27 -0500

    Small changes/fixes

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,53 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from '../components/Header'
 import Head from 'next/head'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const cacheURL = [];
+
+const handleLoadStyle = url => {
+  if (cacheURL.includes(url)) {
+    return;
+  }
+
+  const styleLinks = document.querySelectorAll(
+    'link[href*="/_next/static/css/styles.chunk.css"]'
+  );
+
+  const timestamp = new Date().valueOf();
+
+  [...styleLinks].map(link => {
+    if (link.rel === 'stylesheet') {
+      link.href = `/_next/static/css/styles.chunk.css?v=${timestamp}`;
+      cacheURL.push(url);
+    }
+  });
+};
+
 
 function MyApp({ Component, pageProps }) {
+  const { asPath, events } = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      return;
+    }
+
+    events.on('routeChangeComplete', handleLoadStyle);
+
+    return () => {
+      events.off('routeChangeComplete', handleLoadStyle);
+    };
+  }, [asPath]);
+  
   return <div>
     <Head> 
       <title>Seth Hogue's Portfolio</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     </Head>
+    
     <Header />
     <Component {...pageProps} />
   </div>
--- pages/aboutme.js
+++ pages/aboutme.js
@@ -5,8 +5,8 @@ const aboutme = () => {
         <>
         <div className={`${styles.container}`}>
             <div className={`${styles.centered} shadow-lg`}>
-                <h1 className={styles.title}>About Me</h1>
-                <p className={`${styles.description}shadow-lg`}>Hey, my name is Seth Hogue. I am a student at <a className={styles.link} target="blank" href="https://basecampcodingacademy.org/">Base Camp Coding Academy</a> pursuing a career as a full-stack software engineer. Before BCCA, I had 0 coding experience but during my time at BCCA, I have been excelling in my work and have developed a love for coding. Software development has been great for me because it allows me to express my creativity and solve interesting problems. A fun focus of mine has been creating small game websites such as my Pokemon RPS site built in React and my Quizgo site built with Django. If you're interested in learning more about these projects, feel free to check some of them out in my projects section! I'm also available to chat through LinkedIn or email!</p>
+                <h3 className={styles.title}>About Me</h3>
+                <p className={`${styles.description}`}>Hey, my name is Seth Hogue. I am a student at <a className={styles.link} target="blank" href="https://basecampcodingacademy.org/">Base Camp Coding Academy</a> pursuing a career as a full-stack software engineer. Before BCCA, I had 0 coding experience but during my time at BCCA, I have been excelling in my work and have developed a love for coding. Software development has been great for me because it allows me to express my creativity and solve interesting problems. A fun focus of mine has been creating small game websites such as my Pokemon RPS site built in React and my Quizgo site built with Django. If you're interested in learning more about these projects, feel free to check some of them out in my projects section! I'm also available to chat through LinkedIn or email!</p>
             </div>
         </div>
         <div className={styles.images}>
