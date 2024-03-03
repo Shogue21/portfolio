61bba548c5664ecd0d64ede7be3045d4af385ac6
Seth Hogue <72211190+Shogue21@users.noreply.github.com>
2024-03-03 18:14:42 -0500

    center tag name

--- components/Tag.js
+++ components/Tag.js
@@ -2,7 +2,7 @@ import styles from "../styles/Projects.module.css"
 
 const Tag = ({skill}) => {
     return (
-        <div className={`${styles.tag} ${styles.description} d-flex align-items-center justify-content-center`}>
+        <div className={`${styles.tag} ${styles.description}`}>
             <p>{skill}</p>
         </div>
     )
